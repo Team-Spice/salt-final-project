ca724914cc40963d82a7d2600ac1c556a0201080
Mauro Morales <mauroale2501@gmail.com>
2024-03-25 13:31:24 +0100

    Add Routes to components

--- final-project/src/App.tsx
+++ final-project/src/App.tsx
@@ -1,17 +1,25 @@
-import Nav from './components/Nav'
-import './App.css'
-import Home from './components/Home'
+import Nav from "./components/Nav";
+import "./App.css";
+import Home from "./components/Home";
+import { Route, Routes } from "react-router-dom";
+import SideEffect from "./components/SideEffect";
+import FirstChart from "./components/FirstChart";
+import MainChart from "./components/MainChart";
 
 function App() {
-
   return (
     <>
       <Nav />
       <main>
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/SideEffect" element={<SideEffect />} />
+          <Route path="/FirstChart" element={<FirstChart />} />
+          <Route path="/MainChart" element={<MainChart />} />
+        </Routes>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
