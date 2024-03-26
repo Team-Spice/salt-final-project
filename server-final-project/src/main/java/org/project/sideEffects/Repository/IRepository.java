cc0968c6b98af0f36b74c454401d218cbcbab7e5
Mauro Morales <mauroale2501@gmail.com>
2024-03-26 15:50:25 +0100

    Comment JPA

--- server-final-project/src/main/java/org/project/sideEffects/Repository/IRepository.java
+++ server-final-project/src/main/java/org/project/sideEffects/Repository/IRepository.java
@@ -1,9 +1,9 @@
-package org.project.sideEffects.Repository;
-
-import org.project.sideEffects.Models.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IRepository extends JpaRepository<Product, Long> {
-}
+//package org.project.sideEffects.Repository;
+//
+//import org.project.sideEffects.Models.Product;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface IRepository extends JpaRepository<Product, Long> {
+//}
