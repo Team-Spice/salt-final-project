b9ee4b83c61303af7593e273924c9fbd9469aecc
Mauro Morales <mauroale2501@gmail.com>
2024-03-26 11:05:34 +0100

    Trying fetch to server

--- final-project/src/App.tsx
+++ final-project/src/App.tsx
@@ -1,7 +1,7 @@
 import Nav from "./components/Nav";
 import "./App.css";
 import Home from "./components/Home";
-import { Product } from './types';
+import { Product } from "./types";
 import { Route, Routes } from "react-router-dom";
 import SideEffect from "./components/SideEffect";
 import FirstChart from "./components/FirstChart";
@@ -14,17 +14,40 @@ function App() {
 
   const handleSetProduct = () => {
     setProduct(mockedProduct);
-  }
+  };
+
+  const fetchToServer = async () => {
+    const fetchApi = await fetch(
+      "https://side-effect-resource.azurewebsites.net/api/product"
+    );
+    const data = await fetchApi.text();
+    console.log("data" + data);
+  };
+  fetchToServer();
 
   return (
     <>
       <Nav />
       <main className="bg-slate-300 p-3">
         <Routes>
-          <Route path="/" element={<Home product={product} handleOnClick={handleSetProduct} />} />
-          <Route path="/SideEffect" element={<SideEffect sideEffects={product?.sideEffects} />} />
-          <Route path="/FirstChart" element={<FirstChart product={product} />} />
-          <Route path="/MainChart" element={<MainChart chartData={mockedChartData} />} />
+          <Route
+            path="/"
+            element={
+              <Home product={product} handleOnClick={handleSetProduct} />
+            }
+          />
+          <Route
+            path="/SideEffect"
+            element={<SideEffect sideEffects={product?.sideEffects} />}
+          />
+          <Route
+            path="/FirstChart"
+            element={<FirstChart product={product} />}
+          />
+          <Route
+            path="/MainChart"
+            element={<MainChart chartData={mockedChartData} />}
+          />
         </Routes>
       </main>
     </>
