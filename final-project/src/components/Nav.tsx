be997eade1096e946d835eb779febdcd6cb4213a
= <=>
2024-04-02 16:09:03 +0200

    add favicon

--- final-project/src/components/Nav.tsx
+++ final-project/src/components/Nav.tsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
-import reactLogo from "../assets/react.svg";
+import logo from "../assets/favicon.ico";
 
 const Nav = () => {
   return (
     <nav className="bg-white flex justify-between items-center py-1.5 px-4 sticky mb-2">
       <Link to="/">
-        <img src={reactLogo} alt="logo" />
+        <img src={logo} alt="logo" />
       </Link>
       <Link to="/">
         <button>Home</button>
